1cadd5d93fc551a29752713ac68eda2633bc60c0
Jake Marsden <jakemarsdenjm@gmail.com>
2020-03-03 12:08:43 +1300

    Lexer: Parse more keywords

--- lexer/src/main/java/com/jakemarsden/java/lexer/Keyword.java
+++ lexer/src/main/java/com/jakemarsden/java/lexer/Keyword.java
@@ -3,11 +3,56 @@ package com.jakemarsden.java.lexer;
 import java.util.Arrays;
 
 public enum Keyword {
-  CLASS("class"),
-  PACKAGE("package"),
-  PUBLIC("public"),
-  STATIC("static"),
-  VOID("void");
+  ABSTRACT,
+  ASSERT,
+  BOOLEAN,
+  BREAK,
+  BYTE,
+  CASE,
+  CATCH,
+  CHAR,
+  CLASS,
+  CONST,
+  CONTINUE,
+  DO,
+  DOUBLE,
+  ELSE,
+  EXTENDS,
+  FINAL,
+  FINALLY,
+  FLOAT,
+  FOR,
+  IF,
+  GOTO,
+  IMPLEMENTS,
+  IMPORT,
+  INSTANCEOF,
+  INT,
+  INTERFACE,
+  LONG,
+  NATIVE,
+  NEW,
+  PACKAGE,
+  PRIVATE,
+  PROTECTED,
+  PUBLIC,
+  RETURN,
+  SHORT,
+  STATIC,
+  STRICTFP,
+  SUPER,
+  SWITCH,
+  SYNCHRONIZED,
+  THIS,
+  THROW,
+  THROWS,
+  TRANSIENT,
+  TRY,
+  UNDERSCORE("_"),
+  VAR,
+  VOID,
+  VOLATILE,
+  WHILE;
 
   public static boolean isKeyword(String value) {
     return Arrays.stream(Keyword.values()) //
@@ -16,6 +61,10 @@ public enum Keyword {
   }
 
   private final String value;
+
+  Keyword() {
+    this.value = String.valueOf(this).toLowerCase();
+  }
 
   Keyword(String value) {
     this.value = value;
